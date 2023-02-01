3b1c721a9194ab65f6e2860df66197de8b90a67c
fradotech <fradotech.id@gmail.com>
2023-02-01 09:44:22 +0700

    Add confitional ldap createClient

--- apps/backoffice/src/modules/auth/services/ldap.service.ts
+++ apps/backoffice/src/modules/auth/services/ldap.service.ts
@@ -1,11 +1,14 @@
+import { AuthSchemaEnum } from 'apps/backoffice/src/common/enums/auth.enum';
 import { config } from 'apps/backoffice/src/config';
 import ldap from 'ldapjs';
 
 export class LdapService {
     constructor(private readonly client: any) {
-        this.client = ldap.createClient({
-            url: config.auth.ldap.url,
-        });
+        this.client =
+            config.auth.schema == AuthSchemaEnum.Ldap &&
+            ldap.createClient({
+                url: config.auth.ldap.url,
+            });
     }
 
     async validate(username: string, password: string): Promise<boolean> {
