5c06924a7a9c0097b13d3a5c6f09390b100b73ac
Nemixe <rafel.ami@gmail.com>
2023-02-07 08:56:42 +0700

    remove unused state

--- apps/backoffice/app/Contexts/App.tsx
+++ apps/backoffice/app/Contexts/App.tsx
@@ -5,7 +5,6 @@ export interface IAppContext {
     appState: IAppContextState
 }
 export interface IAppContextState {
-    isReady: boolean;
     isNavigating: boolean;
     isRefreshing: boolean;
 }
@@ -19,16 +18,13 @@ export const AppContext = createContext<Partial<IAppContext>>({})
 
 export const AppProvider: React.FunctionComponent<IAppProviderProps> = (props) => {
     const [state, setState] = useState<IAppContextState>({
-        isReady: false,
         isNavigating: false,
         isRefreshing: false,
     })
 
-    const stateRef = useRef(state);
     const currentPathRef = useRef(window.location.pathname);
 
     const handleSetState = (newState: Partial<IAppContextState>) => {
-        stateRef.current = { ...stateRef.current, ...newState }
         setState((oldState) => ({ ...oldState, ...newState }))
     }
 
@@ -37,7 +33,7 @@ export const AppProvider: React.FunctionComponent<IAppProviderProps> = (props) =
             const isNavigating = currentPathRef.current !== event.detail.visit.url.pathname
             currentPathRef.current = event.detail.visit.url.pathname
 
-            handleSetState({ isReady: true, isRefreshing: true, isNavigating })
+            handleSetState({ isNavigating, isRefreshing: true })
         })
 
         const inertiaFinish = Inertia.on('finish', () => {
