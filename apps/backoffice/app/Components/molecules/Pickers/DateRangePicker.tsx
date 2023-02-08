307e8e1b8a6fc5d586e6f3b8608df2ec3705148f
hudiansyah <hudiansyahrobby7@gmail.com>
2023-02-08 09:55:43 +0800

    refactor: date range picker component

--- apps/backoffice/app/Components/molecules/Pickers/DateRangePicker.tsx
+++ apps/backoffice/app/Components/molecules/Pickers/DateRangePicker.tsx
@@ -2,32 +2,37 @@ import React, { useState } from 'react'
 import dayjs from 'dayjs'
 import type { Dayjs } from 'dayjs'
 import { DatePicker } from 'antd'
+import type { RangePickerProps } from 'antd/es/date-picker';
 
 export type TRangeValue = [Dayjs | null, Dayjs | null] | null;
-export type TDateRangePicker = {
+export type TDateRangePicker = RangePickerProps & {
     range?: number;
     onChange: (val: TRangeValue) => void;
 };
 
-export const DateRangePicker = ({ onChange, range = 7 }: TDateRangePicker) => {
+type TRangePreset = {
+    label: string;
+    value: [Dayjs, Dayjs];
+}[]
+
+const defaultPresets: TRangePreset = [
+    { label: 'Last 7 Days', value: [dayjs().add(-7, 'd'), dayjs()] },
+    { label: 'Last 14 Days', value: [dayjs().add(-14, 'd'), dayjs()] },
+    { label: 'Last 30 Days', value: [dayjs().add(-30, 'd'), dayjs()] },
+    { label: 'Last 90 Days', value: [dayjs().add(-90, 'd'), dayjs()] },
+]
+
+export const DateRangePicker = ({ onChange, range, presets, disabledDate, ...rest }: TDateRangePicker) => {
     const [dates, setDates] = useState<TRangeValue>(null);
     const [value, setValue] = useState<TRangeValue>(null);
 
-    const disabledDate = (current: Dayjs) => {
+    const defaultDisabledDate = (current: Dayjs) => {
         if (!dates) {
             return false;
         }
         const tooLate = dates[0] && current.diff(dates[0], 'days') > range;
         const tooEarly = dates[1] && dates[1].diff(current, 'days') > range;
         return !!tooEarly || !!tooLate;
-    };
-
-    const onOpenChange = (open: boolean) => {
-        if (open) {
-            setDates([null, null]);
-        } else {
-            setDates(null);
-        }
     };
 
     const handleChange = (val) => {
@@ -38,16 +43,11 @@ export const DateRangePicker = ({ onChange, range = 7 }: TDateRangePicker) => {
     return (
 
         <DatePicker.RangePicker value={dates || value}
-            disabledDate={disabledDate}
+            disabledDate={disabledDate || defaultDisabledDate}
             onCalendarChange={(val) => setDates(val)}
             onChange={handleChange}
-            onOpenChange={onOpenChange}
-            presets={[
-                { label: 'Last 7 Days', value: [dayjs().add(-7, 'd'), dayjs()] },
-                { label: 'Last 14 Days', value: [dayjs().add(-14, 'd'), dayjs()] },
-                { label: 'Last 30 Days', value: [dayjs().add(-30, 'd'), dayjs()] },
-                { label: 'Last 90 Days', value: [dayjs().add(-90, 'd'), dayjs()] },
-            ]}
+            presets={presets || defaultPresets}
+            {...rest}
         />
     )
 }
