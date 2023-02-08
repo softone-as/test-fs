c3bd14f7541a18c5b91969f1ac846616f8917958
fradotech <fradotech.id@gmail.com>
2023-02-08 09:56:55 +0700

    Init log activity rest api module

---
+++ apps/backoffice/src/modules/log-activity/applications/log-activity-crud.application.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { ILogActivity } from 'interface-models/log-activity/log-activity.interface';
+import { LogActivityService } from '../services/log-activity.service';
+
+@Injectable()
+export class LogActivityCrudApplication {
+    constructor(private readonly logActivityService: LogActivityService) {}
+
+    async findOneById(id: number): Promise<ILogActivity> {
+        const data = await this.logActivityService.findOneById(id);
+        return data;
+    }
+}
