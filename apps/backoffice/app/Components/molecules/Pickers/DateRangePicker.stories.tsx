829284fcc0f337988884c676d1c10ae98fe6e81b
hudiansyah <hudiansyahrobby7@gmail.com>
2023-02-09 12:23:48 +0800

    feature: setup storybook

---
+++ apps/backoffice/app/Components/molecules/Pickers/DateRangePicker.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { DateRangePicker } from './index';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Molecules/Pickers',
+  component: DateRangePicker,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+} as ComponentMeta<typeof DateRangePicker>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof DateRangePicker> = (args) => <DateRangePicker {...args} />;
+
+export const DateRangePickerMolecule = Template.bind({});
