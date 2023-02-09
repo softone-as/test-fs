59e6d5bbfe633c36946746a6721f0d8444c84abd
Muhammad Firdaus <mbex71@gmail.com>
2023-02-09 18:01:36 +0700

    add logrocket & sentry config

--- apps/backoffice/app/index.js
+++ apps/backoffice/app/index.js
@@ -4,5 +4,30 @@ import App from './App';
 import '../public/unity/css/unity.css';
 import '../public/css/app.css';
 
+import LogRocket from 'logrocket';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+LogRocket.init(process.env.LOGROCKET_APP_ID);
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+});
+
+LogRocket.identify('LOCAL_DEVELOPMENT', {
+    host: process.env.HOST_BACKOFFICE,
+    env: process.env.NODE_ENV,
+});
+
+LogRocket.getSessionURL((sessionURL) => {
+    Sentry.configureScope((scope) => {
+        scope.setExtra('sessionURL', sessionURL);
+    });
+});
+
 // After
 ReactDOM.render(<App />, document.getElementById('app'));
