e81251d03bcfdcaf9308c125c7fde30289aa8960
Muhammad Firdaus <mbex71@gmail.com>
2023-02-10 15:36:51 +0700

    add checking enviroment for logrocket & sentry

--- apps/backoffice/app/index.js
+++ apps/backoffice/app/index.js
@@ -8,26 +8,28 @@ import LogRocket from 'logrocket';
 import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
 
-LogRocket.init(process.env.LOGROCKET_APP_ID);
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [new BrowserTracing()],
+if (process.env.LOGROCKET_APP_ID && process.env.SENTRY_DSN) {
+    LogRocket.init(process.env.LOGROCKET_APP_ID);
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        integrations: [new BrowserTracing()],
 
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
-});
-
-LogRocket.identify('LOCAL_DEVELOPMENT', {
-    host: process.env.HOST_BACKOFFICE,
-    env: process.env.NODE_ENV,
-});
-
-LogRocket.getSessionURL((sessionURL) => {
-    Sentry.configureScope((scope) => {
-        scope.setExtra('sessionURL', sessionURL);
+        // We recommend adjusting this value in production, or using tracesSampler
+        // for finer control
+        tracesSampleRate: 1.0,
     });
-});
+
+    LogRocket.identify('LOCAL_DEVELOPMENT', {
+        host: process.env.HOST_BACKOFFICE,
+        env: process.env.NODE_ENV,
+    });
+
+    LogRocket.getSessionURL((sessionURL) => {
+        Sentry.configureScope((scope) => {
+            scope.setExtra('sessionURL', sessionURL);
+        });
+    });
+}
 
 // After
 ReactDOM.render(<App />, document.getElementById('app'));
