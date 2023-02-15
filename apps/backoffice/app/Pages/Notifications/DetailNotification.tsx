945f1e2eb1f2a75ddc0acb80b0adf3e1868b2bf9
fradotech <fradotech.id@gmail.com>
2023-02-15 16:53:47 +0700

    Not done yet, detail as description usualy

--- apps/backoffice/app/Pages/Notifications/DetailNotification.tsx
+++ apps/backoffice/app/Pages/Notifications/DetailNotification.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Space } from 'antd';
+import { Descriptions, Space, Tag } from 'antd';
 import { defaultSizeSpace } from '../../Utils/theme';
 import { MainLayout } from '../../Layouts/MainLayout';
 import { Breadcrumbs } from '../../Enums/Breadcrumb';
 import { Section } from '../../Components/molecules/Section';
 import { TInertiaProps } from '../../Modules/Inertia/Entities';
 import { IInAppNotification } from 'interface-models/notification/in-app-notification.interface';
+import { DescriptionContainer } from '../../Components/molecules/DescriptionContainer';
 import { formatDate } from '../../Utils/utils';
 
 interface IProps extends TInertiaProps {
@@ -23,11 +24,29 @@ const DetailNotification: React.FC = (props: IProps) => {
                 size={defaultSizeSpace}
                 style={{ width: '100%' }}
             >
-                <Section
-                    title={props.data.title}
-                    extra={formatDate(props.data.createdAt)}
-                >
-                    {props.data.message}
+                <Section>
+                    <DescriptionContainer
+                        size="small"
+                        bordered
+                        column={{ md: 1, xs: 1 }}
+                    >
+                        <Descriptions.Item label="Title">
+                            {props.data.title}
+                        </Descriptions.Item>
+                        <Descriptions.Item label="Message">
+                            {props.data.message}
+                        </Descriptions.Item>
+                        <Descriptions.Item label="Created At">
+                            {formatDate(props.data.createdAt)}
+                        </Descriptions.Item>
+                        <Descriptions.Item label="Is Read">
+                            {props.data.message ? (
+                                <Tag color={'green'}>Read</Tag>
+                            ) : (
+                                <Tag color={'yellow'}>Unread</Tag>
+                            )}
+                        </Descriptions.Item>
+                    </DescriptionContainer>
                 </Section>
             </Space>
         </MainLayout>
