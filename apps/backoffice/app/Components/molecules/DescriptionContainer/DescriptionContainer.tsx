e88308bdbc0b271e7286098462680d23e1f848b9
softone-as <shofwanas12@gmail.com>
2023-02-16 13:37:05 +0700

    fix: bug width when column is set to 1

--- apps/backoffice/app/Components/molecules/DescriptionContainer/DescriptionContainer.tsx
+++ apps/backoffice/app/Components/molecules/DescriptionContainer/DescriptionContainer.tsx
@@ -1,12 +1,39 @@
 import { Descriptions, DescriptionsProps, Grid } from 'antd';
+import { Breakpoint } from 'antd/es/_util/responsiveObserver';
 import React from 'react';
 
 const DescriptionContainer = ({
     size = 'small',
     layout = 'horizontal',
+    column = { lg: 2, md: 2, sm: 1, xs: 1 },
     ...props
 }: DescriptionsProps) => {
-    const { md } = Grid?.useBreakpoint();
+    const { sm, md, lg, xl, xs, xxl } = Grid?.useBreakpoint();
+
+    const isColumnOne = (): string => {
+        if (xxl && (column as Record<Breakpoint, number>).xxl == 1) {
+            return '4%';
+        }
+        if (xl && (column as Record<Breakpoint, number>).xl == 1) {
+            return '4%';
+        }
+
+        if (lg && (column as Record<Breakpoint, number>).lg == 1) {
+            return '4%';
+        }
+
+        if (md && (column as Record<Breakpoint, number>).md == 1) {
+            return '4%';
+        }
+
+        if (sm && (column as Record<Breakpoint, number>).sm == 1) {
+            return '25%';
+        }
+
+        if (xs && (column as Record<Breakpoint, number>).xs == 1) {
+            return '35%';
+        }
+    };
 
     return (
         <Descriptions
@@ -16,19 +43,19 @@ const DescriptionContainer = ({
                 ...props.labelStyle,
                 width:
                     props.bordered && layout == 'horizontal'
-                        ? md && '15%'
+                        ? md && (column as Record<Breakpoint, number>).md != 1
+                            ? '15%'
+                            : isColumnOne()
                         : null,
             }}
             contentStyle={{
-                ...props.labelStyle,
+                ...props.contentStyle,
                 width:
                     props.bordered && layout == 'horizontal'
-                        ? md && '15%'
+                        ? md && '30%'
                         : null,
             }}
-            column={
-                props.column ? props.column : { lg: 2, md: 2, sm: 1, xs: 1 }
-            }
+            column={column}
             {...props}
         >
             {props.children}
