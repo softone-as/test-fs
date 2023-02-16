7d62c25fdf550bc92f0d94ea8b9a707edc2ccecc
softone-as <shofwanas12@gmail.com>
2023-02-16 12:12:48 +0700

    update onOk modal button color

--- apps/backoffice/app/Utils/modal.ts
+++ apps/backoffice/app/Utils/modal.ts
@@ -1,10 +1,18 @@
 import { Modal } from 'antd';
+
 import type { ModalFuncProps } from 'antd';
+import { themeColors } from './theme';
 
 type TPropsModal = ModalFuncProps;
 
 export const useModal = (props: TPropsModal) => {
     return Modal[props.type]({
         ...props,
+        okButtonProps: {
+            style: {
+                backgroundColor: themeColors.primary,
+                borderColor: themeColors.primary,
+            },
+        },
     });
 };
