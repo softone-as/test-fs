cff099f2a86db11c60b9fe9a00478648558729f1
fradotech <fradotech.id@gmail.com>
2023-02-22 10:01:11 +0700

    Move to one file route

--- apps/backoffice/app/Enums/Route.ts
+++ apps/backoffice/app/Enums/Route.ts
@@ -1,12 +1,11 @@
-export type RouteType = {
-    permissions: string[];
-    name: string;
-    href: string;
-    icon?: any;
-};
+export const route = (url: Route, params: Record<string, unknown>): string => {
+    const urlParamIndex = url.indexOf(':');
+    const urlParamName = url.substring(urlParamIndex + 1, url.length);
+    const paramValue = params[urlParamName];
+    const urlNative = url.substring(0, urlParamIndex);
+    const urlFinal = urlNative + paramValue;
 
-export type RouteListType = RouteType & {
-    children?: RouteType[];
+    return urlFinal;
 };
 
 export enum Route {
