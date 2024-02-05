3d82fd0b04a480b9508a0fa141b31c035d04ec36
Nemixe <rafel.ami@gmail.com>
2024-02-05 17:35:19 +0700

    update package.json and adjust client index.js

--- apps/backoffice/app/index.js
+++ apps/backoffice/app/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import 'react-quill/dist/quill.snow.css';
 import '../public/unity/css/unity.css';
 import '../public/css/app.css';
 
 import LogRocket from 'logrocket';
 import * as Sentry from '@sentry/react';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+import { createRoot } from 'react-dom/client';
+import { AppProvider } from './Contexts/App';
+import { ThemeProvider } from './Contexts/Theme';
+import { CookiesProvider } from 'react-cookie';
 
 if (process.env.SENTRY_DSN) {
     const replay = new Sentry.Replay({
-        maskAllText: false, 
+        maskAllText: false,
         maxReplayDuration: 1000 * 60 * 5, // 5 minutes
     });
 
     Sentry.init({
         dsn: process.env.SENTRY_DSN,
-        integrations: [
-            new Sentry.BrowserTracing(),
-            replay
-        ],
+        integrations: [new Sentry.BrowserTracing(), replay],
 
         // We recommend adjusting this value in production, or using tracesSampler
         // for finer control
         tracesSampleRate: +process.env.TRACES_SAMPLE_RATE || 1.0,
-        replaysSessionSampleRate: +process.env.REPLAYS_SESSION_SAMPLE_RATE || 0.3,
-        replaysOnErrorSampleRate: +process.env.REPLAYS_ON_ERROR_SAMPLE_RATE || 1.0,
+        replaysSessionSampleRate:
+            +process.env.REPLAYS_SESSION_SAMPLE_RATE || 0.3,
+        replaysOnErrorSampleRate:
+            +process.env.REPLAYS_ON_ERROR_SAMPLE_RATE || 1.0,
     });
 
     replay.start();
@@ -44,6 +47,18 @@ if (process.env.LOGROCKET_APP_ID) {
         });
     });
 }
-
-// After
-ReactDOM.render(<App />, document.getElementById('app'));
+createInertiaApp({
+    resolve: (name) => require(`./Pages/${name}`),
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    setup({ el, App, props }) {
+        createRoot(el).render(
+            <CookiesProvider>
+                <ThemeProvider>
+                    <AppProvider>
+                        <App {...props} />
+                    </AppProvider>
+                </ThemeProvider>
+            </CookiesProvider>,
+        );
+    },
+});
