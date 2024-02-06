50709b54064538f25ac6c726ac26356f6f4bfac9
Nemixe <rafel.ami@gmail.com>
2024-02-06 10:06:42 +0700

    rever

--- apps/backoffice/app/index.js
+++ apps/backoffice/app/index.js
@@ -15,15 +15,16 @@ if (process.env.SENTRY_DSN) {
 
     Sentry.init({
         dsn: process.env.SENTRY_DSN,
-        integrations: [new Sentry.BrowserTracing(), replay],
+        integrations: [
+            new Sentry.BrowserTracing(),
+            replay
+        ],
 
         // We recommend adjusting this value in production, or using tracesSampler
         // for finer control
         tracesSampleRate: +process.env.TRACES_SAMPLE_RATE || 1.0,
-        replaysSessionSampleRate:
-            +process.env.REPLAYS_SESSION_SAMPLE_RATE || 0.3,
-        replaysOnErrorSampleRate:
-            +process.env.REPLAYS_ON_ERROR_SAMPLE_RATE || 1.0,
+        replaysSessionSampleRate: +process.env.REPLAYS_SESSION_SAMPLE_RATE || 0.3,
+        replaysOnErrorSampleRate: +process.env.REPLAYS_ON_ERROR_SAMPLE_RATE || 1.0,
     });
 
     replay.start();
