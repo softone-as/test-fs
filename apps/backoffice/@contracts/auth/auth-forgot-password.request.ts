3cd7972016a4b1c37bedb38392239d4bfa707781
fradotech <fradotech.id@gmail.com>
2024-02-06 11:22:29 +0700

    refactor: delete implement zod schema

--- apps/backoffice/@contracts/auth/auth-forgot-password.request.ts
+++ apps/backoffice/@contracts/auth/auth-forgot-password.request.ts
@@ -1,9 +1,6 @@
 import { createZodDto } from 'nestjs-zod';
-import {
-    AuthForgotPasswordSchema,
-    TAuthForgotPasswordSchema,
-} from './auth-forgot-password.schema';
+import { AuthForgotPasswordSchema } from './auth-forgot-password.schema';
 
-export class AuthForgotPasswordRequest
-    extends createZodDto(AuthForgotPasswordSchema)
-    implements TAuthForgotPasswordSchema {}
+export class AuthForgotPasswordRequest extends createZodDto(
+    AuthForgotPasswordSchema,
+) {}
