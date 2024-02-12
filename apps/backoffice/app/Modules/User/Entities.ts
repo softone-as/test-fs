079d0e939577c61274bddeb4425f376f306f329d
Daniel Prastha <danielprastha4@gmail.com>
2024-02-12 11:41:58 +0700

    restore code

--- apps/backoffice/app/Modules/User/Entities.ts
+++ apps/backoffice/app/Modules/User/Entities.ts
@@ -1,11 +1,11 @@
-import { ProfileDetailResponse } from 'aws-sdk/clients/rolesanywhere';
 import { UserCreateRequest } from '../../../src/modules/iam/requests/user-create.request';
 import { UserResponse } from '../../../src/modules/iam/responses/user.response';
+import { ProfileResponse } from '../../../src/modules/profile/responses/profile.response';
 
 type IUser = UserResponse;
 
 type IUserForm = Omit<UserCreateRequest, 'roles'>;
 
-type IProfile = ProfileDetailResponse;
+type IProfile = ProfileResponse;
 
 export { IUser, IProfile, IUserForm };
