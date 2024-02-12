5eb305716e200c84f4f547ab186a8a01cfbf1f3b
fradotech <fradotech.id@gmail.com>
2024-02-12 17:12:15 +0700

    refactor: assign type without omit

--- apps/backoffice/app/Modules/Role/Entities.ts
+++ apps/backoffice/app/Modules/Role/Entities.ts
@@ -1,5 +1,5 @@
 import { RoleCreateRequest } from 'apps/backoffice/src/modules/iam/requests/role-create.request';
 
-type IRoleForm = Omit<RoleCreateRequest, ''>;
+type IRoleForm = RoleCreateRequest;
 
 export { IRoleForm };
