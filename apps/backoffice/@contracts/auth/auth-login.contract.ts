4d1e276fff8997363aa73c5b68ac5116ea42cee0
Busan <94603251+idubsantoso@users.noreply.github.com>
2024-02-16 16:27:03 +0700

    implement zod in login and remove yup module

---
+++ apps/backoffice/@contracts/auth/auth-login.contract.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const AuthLoginSchema = z.object({
+    email: z
+        .string({
+            required_error: 'Field Email wajib diisi',
+        })
+        .email({
+            message: 'Field Email wajib berformat email',
+        }),
+    password: z.string({
+        required_error: 'Field Password wajib diisi',
+    }),
+});
+
+export type TAuthLoginSchema = z.infer<typeof AuthLoginSchema>;
