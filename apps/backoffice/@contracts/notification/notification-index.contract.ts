2ba0287ea4937c020634bfdd06e24c29f989d92e
Daniel Prastha <danielprastha4@gmail.com>
2024-02-19 06:03:28 +0700

    feat: notification index & detail shared props

---
+++ apps/backoffice/@contracts/notification/notification-index.contract.ts
@@ -0,0 +1,4 @@
+import { IPaginateResponse } from 'apps/backoffice/src/common/interface/index.interface';
+import { IInAppNotification } from 'interface-models/notification/in-app-notification.interface';
+
+export type TCNotificationIndexProps = IPaginateResponse<IInAppNotification>;
