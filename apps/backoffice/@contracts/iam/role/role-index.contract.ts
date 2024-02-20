3ac0be24cf96181a959c06d37d454338304fae2d
Daniel Prastha <danielprastha4@gmail.com>
2024-02-20 17:01:03 +0700

    feat: Role index schema

--- apps/backoffice/@contracts/iam/role/role-index.contract.ts
+++ apps/backoffice/@contracts/iam/role/role-index.contract.ts
@@ -1,4 +1,10 @@
 import { IPaginateResponse } from 'apps/backoffice/src/common/interface/index.interface';
+import { IndexRequestSchema } from 'apps/backoffice/src/common/request/index.request';
 import { IRole } from 'interface-models/iam/role.interface';
+import { z } from 'zod';
+
+export const RoleIndexSchema = IndexRequestSchema.extend({});
+
+export type TRoleIndexSchema = z.infer<typeof RoleIndexSchema>;
 
 export type TCRoleIndexProps = IPaginateResponse<IRole>;
