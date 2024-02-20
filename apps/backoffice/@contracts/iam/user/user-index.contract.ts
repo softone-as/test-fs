d14c3dde60fa0414d391d8874a4a01fc60014bf2
Daniel Prastha <danielprastha4@gmail.com>
2024-02-20 16:52:47 +0700

    fix: gender enum

--- apps/backoffice/@contracts/iam/user/user-index.contract.ts
+++ apps/backoffice/@contracts/iam/user/user-index.contract.ts
@@ -1,10 +1,11 @@
+import { GenderEnum } from 'apps/backoffice/app/Common/Enums/Gender.enum';
 import { IPaginateResponse } from 'apps/backoffice/src/common/interface/index.interface';
 import { IndexRequestSchema } from 'apps/backoffice/src/common/request/index.request';
 import { IUser } from 'interface-models/iam/user.interface';
 import { z } from 'zod';
 
 export const UserIndexSchema = IndexRequestSchema.extend({
-    gender: z.string().optional(),
+    gender: z.nativeEnum(GenderEnum).optional(),
     createdAt: z.string().optional(),
 });
 
