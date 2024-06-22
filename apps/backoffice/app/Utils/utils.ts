41910ff455787598a730bcf5771a0b01a750c33b
softone-as <shofwanas12@gmail.com>
2024-06-22 11:54:06 +0700

    feat: utils for check permission and format image

--- apps/backoffice/app/Utils/utils.ts
+++ apps/backoffice/app/Utils/utils.ts
@@ -2,6 +2,8 @@ import { useMediaQuery } from 'react-responsive';
 import { TErrorProps, TValidationError } from '../Modules/Inertia/Entities';
 import { notification } from 'antd';
 import { format } from 'date-fns';
+import { TMDB_IMAGE_URL } from '../Modules/TMDB/constants';
+import { IPermission } from 'interface-models/iam/permission.interface';
 
 export const isMobileScreen = (): boolean => {
     return useMediaQuery({ query: '(max-width: 767px)' });
@@ -46,4 +48,23 @@ export const debounce = (
         clearTimeout(timer);
         timer = setTimeout(() => fn(...args), delay);
     };
+};
+
+export const truncate = (str: string, n: number): string => {
+    return str.length > n ? str.slice(0, n - 1) + '...' : str;
+};
+
+export const formatUrlImage = (url: string, isTMDB?: boolean): string => {
+    const localUrl = `/${url.split('/').pop()}`;
+    return isTMDB ? `${TMDB_IMAGE_URL}/${url}` : localUrl;
+};
+
+export const isPermissionExist = (
+    permissions: IPermission[],
+    permission: string | string[],
+): boolean => {
+    if (typeof permission === 'string') {
+        return permissions?.some((item) => item.key === permission);
+    }
+    return permissions?.some((item) => permission.includes(item.key));
 };
